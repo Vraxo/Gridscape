d24a6a9d7cb41737dca78702f683d0226d4e296e
Parsa <Parsa@DESKTOP-32S8QLS>
2024-07-15 21:43:25 +0330

    Made sliders smaller inside item lists

--- Gridscape/Source/A - Nodex/Nodes/ItemList.Scene.cs
+++ Gridscape/Source/A - Nodex/Nodes/ItemList.Scene.cs
@@ -9,9 +9,9 @@ partial class ItemList : ClickableRectangle
             OnUpdate = (slider) =>
             {
                 slider.Position.X = Size.X - Origin.X - slider.Size.X - slider.Origin.X;
-                slider.Position.Y = -Origin.Y + slider.MiddleButton.Radius;
+                slider.Position.Y = -Origin.Y + slider.MiddleButton.Radius * 4;
 
-                slider.Size = new(slider.Size.X, Size.Y - slider.MiddleButton.Radius * 2);
+                slider.Size = new(slider.Size.X, Size.Y - slider.MiddleButton.Radius * 8);
             }
         });
     }
